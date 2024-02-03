3b174a7fc727d395e2966383ca0c8933ee938336
Supratim <supratimsarkar1711@gmail.com>
2024-02-03 19:33:29 +0530

    Implementing Contact Page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styles from './contact.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const ContactPage = () => {
   return (
-    <div>
-      Contact Page
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="contact" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Let's Get In Touch</h1>
+        <div className={styles.connectLinks}>
+          <Link 
+          className={styles.link}
+          href={"https://github.com/SupratimSarkar2001"} target='_blank'>
+            <Image src="/github.png" alt="githbub" className={styles.img_logo} width={40} height={40} />
+          </Link>
+          <Link
+          className={styles.link}
+           href={"https://www.linkedin.com/in/supratimsarkar08/"} target='_blank'>
+            <Image src="/linkedin.png" alt="linkedin" className={styles.img_logo} width={40} height={40} />
+          </Link>
+          <Link 
+          className={styles.link}
+          href={"https://twitter.com/8ssarkar"} target='_blank'>
+            <Image src="/x.png" alt="x" className={styles.img_logo} width={40} height={40} />
+          </Link>
+        </div>
+        <h3 className={styles.subtitle}>Email Me on: <span className={styles.email}>supratimsarkar2001@gmail.com</span></h3>
+      </div>
     </div>
   )
 }
